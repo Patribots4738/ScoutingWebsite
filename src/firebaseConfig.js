abc6b20ceeb8383195701684991a713f2f507d4e
Lucas Williams <surfluke19@gmail.com>
2025-09-19 21:52:01 -0700

    wrong var name sorry sorry

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: process.env.APIKEY,
     authDomain: process.env.AUTHDOMAIN,
     databaseURL: process.env.DATABASEURL,
-    projectId: process.env.PROJECT_ID,
+    projectId: process.env.PROJECTID,
     storageBucket: process.env.STORAGEBUCKET,
     messagingSenderId: process.env.MESSAGINGSENDERID,
     appId: process.env.APPID,
