363b2b82e7687df03a2263b0529224fb01f091f8
Tina H <148731136+derp00monsta@users.noreply.github.com>
2025-09-26 23:16:31 -0700

    change event id for chezy

--- src/Container.js
+++ src/Container.js
@@ -58,7 +58,7 @@ class Container extends React.Component {
     var sendData = data[1]
     data = data[0]
 
-    const eventID = '2025test';
+    const eventID = '2025CASAN1';
 
     if (sendData) {
       let validMatch = true;
